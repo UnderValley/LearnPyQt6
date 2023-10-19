7d510056b2e7744b5a8d26210e97fe2d79604d43
yundou <954669575@qq.com>
2023-10-19 23:04:10 +0800

    My first windows

---
+++ FirstWindow/PartOne/Windowfour.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QIcon
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(200, 200, 700, 400)
+        self.setWindowTitle("Python GUI Development")
+        self.setWindowIcon(QIcon())
+
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
